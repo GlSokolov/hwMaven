7f46b3834160cf859de5fb5d847c6a0eb983ed86
gl_sokolov <gl_sokolov@mail.ru>
2023-01-08 12:57:45 +0300

    Домашка выполнена

--- src/main/java/com/example/hwmaven_budgetapp/controllers/FirstController.java
+++ src/main/java/com/example/hwmaven_budgetapp/controllers/FirstController.java
@@ -4,14 +4,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class FirstController {
+    String nameOfProject = "First Maven project";
+
 @GetMapping
     public String helloWeb(){
-        return "Hello web";
+        return "Приложение запущено";
     }
-@GetMapping("/page")
-    public String test(@RequestParam String page){
-        return "Hello page " + page;
+@GetMapping("/info")
+    public String info(@RequestParam String name){
+        return "Имя ученика - " + name +
+                " Название проекта - " + nameOfProject +
+                " Дата создания проекта - " + LocalDateTime.now() +
+                " Описание проекта - Проект \"hwMaven\".\n" +
+                "\t\tФункционал проекта пока ограничен выводом в брауезере инфромации о самом проекте, в будущем, возможно, функционал будет расширяться.\n" +
+                "\t\tПроект написан на языке Java и реализован с помощью фреймворка Spring и соответствующих ему библиотек." ;
     }
 }
